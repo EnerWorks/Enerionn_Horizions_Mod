ef3a808cb6165d3f3b82bab047fa711cac5d5541
EnerWorks <rizeeddin@gmail.com>
2026-03-11 08:32:51 +0300

    Change Some small things on fabric.mod.json

---
+++ src/main/java/net/enerionn/horizons/EnerionnHorizonsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.enerionn.horizons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnerionnHorizonsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
